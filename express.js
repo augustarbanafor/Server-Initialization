645471e6f2cb99aeb0488ac5f8bba2854c5b5b34
augustarbanafor <augustarbanafor@gmail.com>
2022-05-26 11:39:07 +0200

    routing with express framework

--- express.js
+++ express.js
@@ -5,5 +5,14 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./routes', {root:__dirname})
+    res.sendFile('./routes/index.html', {root:__dirname})
+})
+app.get('/about', (req, res) => {
+    res.sendFile('./routes/about.html', {root:__dirname})
+})
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+app.use((req, res) => {
+    res.sendFile('./routes/404.html', {root:__dirname})
 })
